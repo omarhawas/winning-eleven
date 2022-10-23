56e9121eb88442e0791ad3dd3e3e0e15e00321b4
Omar Hawas <omarhawas94@gmail.com>
2022-10-22 20:15:15 -0400

    added player cards

--- src/components/PlayerCards.js
+++ src/components/PlayerCards.js
@@ -1,12 +1,32 @@
 import React from "react";
+import Card from "react-bootstrap/Card";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 const PlayerCards = ({ players }) => {
   return (
-    <div>
+    <Row>
       {players.map((player) => {
-        return <div key={player.full_name}>{player.full_name}</div>;
+        return (
+          <Col xs={2} key={player.full_name}>
+            <Card style={{ width: "6rem" }}>
+              <Card.Img
+                variant="top"
+                src="https://esports-news.co.uk/wp-content/uploads/2016/09/anonymous-guy-1.jpg"
+              />
+              <Card.Body>
+                <Card.Title style={{ fontSize: "12px" }}>
+                  {player.full_name}
+                </Card.Title>
+                <Card.Text style={{ fontSize: "9px" }}>
+                  {player.position} {player["Current Club"]}
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+        );
       })}
-    </div>
+    </Row>
   );
 };
 
